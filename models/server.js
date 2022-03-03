0e85df75c904461d275a2ee4c7c12c37b75b5efa
Alejandro <urielcrow@gmail.com>
2022-03-02 18:17:10 -0600

    url actualizado 6

--- models/server.js
+++ models/server.js
@@ -16,7 +16,15 @@ class Server{
 
     middlewares(){
         this.app.use( express.static( path.resolve( __dirname,'../public' ) ) );
-        this.app.use( cors() );
+        this.app.use( 
+            (req, res, next) => {
+                res.header('Access-Control-Allow-Origin', '*');
+                res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+                res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+                res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+                next();
+            }
+         );
     }
 
     socketsInit(){
